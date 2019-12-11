ae97c4b4b1358e60f01afb8af99a6eda4bdf4dcf
rtnxs <riotnexus@mail.ru>
2019-12-11 13:49:11 +0300

    added devise, edit views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  helper_method :current_user_can_edit?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+        :account_update,
+        keys: [:password, :password_confirmation, :current_password]
+    )
+  end
+
+  def current_user_can_edit?(event)
+    user_signed_in? && event.user == current_user
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,21 @@
 class User < ApplicationRecord
-  EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  #EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i #TODO
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   has_many :events
 
   validates :name, presence: true, length: {maximum: 35}
-  validates :email, presence: true, uniqueness: true, length: {maximum: 255}, format: {with: EMAIL_REGEXP}
+  #validates :email, presence: true, uniqueness: true, length: {maximum: 255}, format: {with: EMAIL_REGEXP} #TODO
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "Usver" if self.name.blank?
+  end
 end
