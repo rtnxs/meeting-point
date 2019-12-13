4549e0d7cbd05edbd8c7459b3af97e1c50fa5164
rtnxs <riotnexus@mail.ru>
2019-12-13 15:06:44 +0300

    added flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -19,10 +18,7 @@ require("jquery")
 
 import 'bootstrap'
 import '../stylesheets/application.scss'
+import 'plugins/flatpickr'
 
-import flatpickr from "flatpickr";
-import 'flatpickr/dist/flatpickr.min.css'
 
-flatpickr(".datepicker", {
 
-})
---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,11 @@
+import flatpickr from "flatpickr"
+require("flatpickr/dist/flatpickr.css")
+
+document.addEventListener("turbolinks:load", () => {
+    flatpickr("[data-behavior='flatpickr']", {
+        enableTime: true,
+        time_24hr: true,
+        dateFormat: "Y-m-d H:i",
+        minDate: "today",
+    })
+})
