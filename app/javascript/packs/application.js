da9fc7331d66857f0046faf84bffdb9929c26bb2
rtnxs <riotnexus@mail.ru>
2019-12-13 11:29:34 +0300

    flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -18,3 +19,10 @@ require("channels")
 
 import 'bootstrap'
 import '../stylesheets/application.scss'
+
+import flatpickr from "flatpickr";
+import 'flatpickr/dist/flatpickr.min.css'
+
+flatpickr(".datepicker", {
+
+})
