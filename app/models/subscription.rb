ccedbf5921b30fcae88d682c43d53a10a2f838e5
rtnxs <riotnexus@mail.ru>
2019-12-16 18:46:07 +0300

    delete subscribe button for event.user, check email subscribers

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,9 +4,11 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  validates :user_name, presence: true, unless: -> { user.present? }
-  validates :user_email, presence: true, format: EMAIL_REGEXP, unless: -> { user.present? }
   validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+  validate :can_subscribe?, on: :create, if: -> { user.present? }
+  validates :user_name, presence: true, unless: -> { user.present? }
+  validate :email_used?, on: :create, unless: -> { user.present? }
+  validates :user_email, presence: true, format: EMAIL_REGEXP, unless: -> { user.present? }
   validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
 
   def user_name
@@ -15,5 +17,11 @@ class Subscription < ApplicationRecord
 
   def user_email
     user.present? ? user.email : super
+  end
+
+  private
+
+  def email_used?
+    errors.add(:user_email, I18n.t('controllers.subscriptions.email_used')) if User.where(email: user_email).present?
   end
 end
