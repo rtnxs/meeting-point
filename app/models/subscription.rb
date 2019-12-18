35d030454388f772a981fe9cea2a70225164f478
rtnxs <riotnexus@mail.ru>
2019-12-18 15:58:58 +0300

    added validates when user subs to his event, edit ru.yml

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,6 +5,7 @@ class Subscription < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+  validate :can_subscribe?, on: :create, if: -> { user.present?}
   validates :user_name, presence: true, unless: -> { user.present? }
   validate :email_used?, on: :create, unless: -> { user.present? }
   validates :user_email, presence: true, format: EMAIL_REGEXP, unless: -> { user.present? }
@@ -21,6 +22,10 @@ class Subscription < ApplicationRecord
   private
 
   def email_used?
-    errors.add(:user_email, I18n.t('controllers.subscriptions.email_used')) if User.where(email: user_email).present?
+    errors.add(:user_email, I18n.t('errors.messages.subscription.email_used')) if User.where(email: user_email).present?
+  end
+
+  def can_subscribe?
+    errors.add(:user, I18n.t('errors.messages.subscription.cant_subs')) if event.user == user
   end
 end
