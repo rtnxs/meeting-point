4d87dc8c237cd3fb8a99cbded5ead306f7691eeb
rtnxs <riotnexus@mail.ru>
2019-12-21 14:47:56 +0300

    actionmailer, model EventMailer, edit controllers comment/subscr views, config, localization

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,7 @@ class CommentsController < ApplicationController
     @new_comment.user = current_user
 
     if @new_comment.save
+      notify_subscribers(@event, @new_comment)
       redirect_to @event, notice: t('controllers.comments.created')
     else
       render 'events/show', alert: t('controllers.comments.error')
@@ -40,5 +41,13 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:body, :user_name)
     #params.fetch(:comment, {})
+  end
+
+  def notify_subscribers(event, comment)
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+
+    all_emails.each do |mail|
+      EventMailer.comment(event, comment, mail).deliver_now
+    end
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -61,4 +61,13 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: '587',
+      user_name: '',
+      password: '',
+      authentication: 'plain',
+      enable_starttls_auto: true
+  }
 end
