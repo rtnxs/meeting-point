f2605ef633a3b95d9d63ee66877ea2a7a561430a
rtnxs <riotnexus@mail.ru>
2019-12-23 18:56:07 +0300

    add pincode to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
   before_action :set_event, only: [:show]
   before_action :set_current_user_event, only: [:edit, :update, :destroy]
+  before_action :password_guard!, only: [:show]
 
   # GET /events
   def index
@@ -60,8 +61,25 @@ class EventsController < ApplicationController
     @event = current_user.events.find(params[:id])
   end
 
+  def password_guard!
+    return true if @event.pincode.blank?
+    return true if signed_in? && current_user == @event.user
+
+    if params[:pincode].present? && @event.pincode_valid?(params[:pincode])
+      cookies.permanent["events_#{@event.id}_pincode"] = params[:pincode]
+    end
+
+    pincode = cookies.permanent["events_#{@event.id}_pincode"]
+    unless @event.pincode_valid?(pincode)
+      if params[:pincode].present?
+        flash.now[:alert] = I18n.t('controllers.events.wrong_pincode')
+      end
+      render 'password_form'
+    end
+  end
+
   def event_params
     #params.fetch(:event, {}).permit(:title, :address, :datetime, :description)
-    params.require(:event).permit(:title, :address, :datetime, :description)
+    params.require(:event).permit(:title, :address, :datetime, :description, :pincode)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_20_124611) do
+ActiveRecord::Schema.define(version: 2019_12_23_151534) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_12_20_124611) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
