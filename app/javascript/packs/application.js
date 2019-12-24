ab690958de3cd2a4541f07f1f45895d8ee34257f
rtnxs <riotnexus@mail.ru>
2019-12-24 11:30:49 +0300

    added lightbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -21,6 +20,4 @@ import '../stylesheets/application.scss'
 import 'plugins/flatpickr'
 import '@fortawesome/fontawesome-free/js/all'
 import 'plugins/maps'
-
-
-
+import 'plugins/lightbox'
---
+++ app/javascript/plugins/lightbox.js
@@ -0,0 +1,6 @@
+import './ekko-lightbox'
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
